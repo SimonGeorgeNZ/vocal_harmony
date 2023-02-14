3f69fe1f54f237f2f6d9f06b3c9583fa8e9bb5fe
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2023-02-14 09:44:09 +0000

    flask working and python file previewing

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'CocktailTraining'
-app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/CocktailTraining?retryWrites=true&w=majority'
+# app.config["MONGO_DBNAME"] = 'CocktailTraining'
+# app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/CocktailTraining?retryWrites=true&w=majority'
 
+# mongo = PyMongo(app)
 
-mongo = PyMongo(app)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
+
