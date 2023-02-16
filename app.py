44fecbeca2d47159e19f3c9cca44a8926d30695b
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2023-02-16 01:35:30 +0000

    Dropdown key list button working

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-# app.config["MONGO_DBNAME"] = 'CocktailTraining'
-# app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/CocktailTraining?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'vocal_harmony'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/vocal_harmony?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    keySig = list(mongo.db.keys.find().sort("_id"))
+
+    return render_template('index.html', ks=keySig)
 
 if __name__ == '__main__':
     app.run()
