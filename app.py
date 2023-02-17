c892e10926c6a5858485dd9906758188cde9e246
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2023-02-17 18:05:05 +0000

    Pre making env file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'vocal_harmony'
 app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/vocal_harmony?retryWrites=true&w=majority'
